5ae1dff93fd19a26c5b5fc44f395e61b07689b54
NGOCHOABUI <buingochoa1987@gmail.com>
2019-08-01 11:07:19 +1000

    Add Basket Service

---
+++ Shop/Shop.Core/Contracts/IBasketService.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Web;
+using Shop.Core.ViewModels;
+
+namespace Shop.Core.Contracts
+{
+    public interface IBasketService
+    {
+        void AddToBasket(HttpContextBase httpContext, string productId);
+        BasketSummaryViewModel GetBacketSummary(HttpContextBase httpContext);
+        List<BasketItemViewModel> GetBasketItems(HttpContextBase httpContext);
+        void RemoveFromBasket(HttpContextBase httpContext, string itemId);
+    }
+}
